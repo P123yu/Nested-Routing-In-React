cd15e6c9f6033337641fa022ac2dcef2f15e3db5
Piyush Kumar Singh <96623633+P123yu@users.noreply.github.com>
2025-07-22 10:24:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Dashboard from './Dashboard'
-import C from './C.JSX'
+import C from './C.JSX'  // by mistake this is the component name here compleletely C.JSX is treated here as a compponent 
 import B from './B'
 import A from './A'
 
